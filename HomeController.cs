6c1829206f8fee5acf4651d6adeb7be79476263e
Muhammad Rameez <77886136+ZapeeoSheikh@users.noreply.github.com>
2022-01-29 23:10:47 +0500

    Assignment 09

--- HomeController.cs
+++ HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WebApplication2.Controllers
+namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
